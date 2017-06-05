4451d69c58abe13f11ef74ea3e43c246c43b7ec9
Danielle Westerman <daniellepwesterman@gmail.com>
2017-06-05 18:48:42 -0400

    end of day sequelize models

---
+++ server/models/index.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+
+const users = require('./users');
+const products = require('./products');
+const reviews = require('./reviews');
+const orders = require('./orders');
+
+// const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/grace-shopper', {
+//   logging: false, // unless you like the logs
+//   // ...and there are many other options you may want to play with
+//   force: true
+// });
+
+
+// // create relationships here
+
+// module.exports = db;
---
+++ server/models/orders.js
@@ -0,0 +1,15 @@
+const db = require('../db');
+const Sequelize = require('sequelize');
+
+module.exports = db.define('order', {
+	status: {
+		type: Sequelize.ENUM('Created', 'Processing', 'Cancelled', 'Completed')
+	},
+	datePlaced: {
+		type: Sequelize.DATE
+	}
+})
+
+
+// one user to many orders
+// many products to many order
