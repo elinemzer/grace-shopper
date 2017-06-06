8404cfe9dced5a73766e43e8a0b74a816d1e6c88
Jeremy Wicks <artfullu@gmail.com>
2017-06-06 15:32:27 -0400

    Removing bundle.js.map tracking

--- browser/index.js
+++ browser/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, Route, hashHistory, IndexRedirect, IndexRoute } from 'react-router';
 import store from './store';
-import scss from '../index.scss';
+// import scss from '../index.scss';
 // import db from '../server/models';
 
 import Landing from './components/Landing'
