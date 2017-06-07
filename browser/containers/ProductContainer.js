9ee968e251109f108a3582bc238ef4934779caf2
Jeremy Wicks <artfullu@gmail.com>
2017-06-07 11:44:25 -0400

    Updated Seed File and Created Single Product View. Fixed Some issues with routing

--- browser/containers/ProductContainer.js
+++ browser/containers/ProductContainer.js
@@ -1,1 +1,13 @@
-ProductContainer.js
+import React, {Component} from 'react';
+import store from '../store';
+import Product from '../components/Product';
+import {connect} from 'react-redux';
+
+
+const mapStateToProps = function(state){
+	return {
+		product: state.selectedProduct
+	}
+}
+
+export default connect(mapStateToProps)(Product)
--- server/api/products.js
+++ server/api/products.js
@@ -2,26 +2,33 @@ const router = require('express').Router();
 const Products = require('../models/products');
 
 //taking supplied product id and attaching product object to request
-router.param('product', function(req, res, next, id){
-  Products.findById(id)
-  .then(product => {req.product = product})
-})
 
-// matches GET requests to /api/products/
-router.get('/', function (req, res, next){
-  Products.findAll()
-  .then(productsFound => res.send(productsFound))
-  .catch(next)
-});
+// router.param('product', function(req, res, next, id){
+//   Products.findById(id)
+//   .then(product => {req.product = product})
+// })
 
 //needed a db query
 router.get('/:productId', function (req, res, next){
+
   Products.findById(req.params.productId)
   .then(productFound => {
+    console.log('in route with', productFound)
     res.send(productFound)
   })
   .catch(next)
 });
+
+// matches GET requests to /api/products/
+router.get('/', function (req, res, next){
+  Products.findAll()
+  .then(productsFound => {
+
+    res.send(productsFound)})
+  .catch(next)
+});
+
+
 
 // matches POST requests to /api/products/
 router.post('/', function (req, res, next){
