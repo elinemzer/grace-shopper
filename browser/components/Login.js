d02e87d8a4e779ea13cae7920c69927735d38ec7
Emily Grace Acres <acres.emilygrace@gmail.com>
2017-06-08 17:58:31 -0400

    added new data to seed file and styled forms

--- browser/components/Login.js
+++ browser/components/Login.js
@@ -2,43 +2,51 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default function Login (props) {
-	
+
 	console.log(props)
 
   return (
-  	<div className='default-container'>
+  	<div className='login-container'>
     	<div className="row">
-    		<div className='col-xs-6'>
-    			<h1>Returning User? Sign in here!</h1>
+    		<div className='col-xs-5' id="line">
+    			<h1 className="fancy-type form-title">Returning User?</h1>
+          <h1 className="form-subtitle">Sign in</h1>
 				<form onSubmit={props.loginUser}>
-					<p>User Name</p>
+					<p>Email</p>
 						<input id="a1" type="text" className="form-control col-md-2" onChange={props.handleEmail} aria-describedby="basic-addon1" />
 					<p>Password</p>
 						<input id="a2" type="text" className="form-control col-md-2" onChange={props.handlePassword} aria-describedby="basic-addon1" />
 					<span className="input-group-btn">
-					   	<button className="btn btn-default" type="submit">Sign In</button>
+					   	<button className="btn btn-default login-btn" type="submit">Sign In</button>
 				    </span>
 			    </form>
 		    </div>
-		<div className='col-xs-6'>
-    			<h1>New User? GET LOST!</h1>
+    <div className="col-md-2">
+    </div>
+		<div className='col-xs-5 signup'>
+    			<h1 className="fancy-type form-title">New User?</h1>
+          <h1 className="form-subtitle">Sign Up</h1>
 				<form onSubmit= {props.signUpUser} >
-					<p>First Name</p>
-						<input id="a1" type="text" className="form-control col-md-2" onChange={props.handleFirst} aria-describedby="basic-addon1" />
+          <div className="signup-names">
+  					<p>First Name</p>
+  						<input id="a1" type="text" className="form-control col-md-2" onChange={props.handleFirst} aria-describedby="basic-addon1" />
+          </div>
+          <div className="signup-names" id="last-name">
 					<p>Last Name</p>
 						<input id="a2" type="text" className="form-control col-md-2" onChange={props.handleLast} aria-describedby="basic-addon1" />
+          </div>
 					<p>Email</p>
 						<input id="a1" type="text" className="form-control col-md-2" onChange={props.handleEmail} aria-describedby="basic-addon1" />
 					<p>Password</p>
 						<input id="a2" type="text" className="form-control col-md-2" onChange={props.handlePassword} aria-describedby="basic-addon1" />
 
 					<span className="input-group-btn">
-					   	<button className="btn btn-default" type="submit">Sign Up</button>
+					   	<button className="btn btn-default login-btn" type="submit">Sign Up</button>
 				    </span>
 			    </form>
 		    </div>
     	</div>
-        
+
     </div>
   	)
 }
--- browser/components/Product.js
+++ browser/components/Product.js
@@ -11,21 +11,21 @@ export default function AllProducts (props) {
   	<div className="default-container">
       <div className="row">
 
-          <div className="col-xs-6" key={ fish.id }>
-              <img src={ fish.imageUrl } />
+          <div className="col-xs-5" key={ fish.id }>
+              <img id="single-product-img" src={ fish.imageUrl } />
 
           </div>
           <div className="caption col-xs-6">
             <h2 id="product-title" className="fancy-type">{fish.title}</h2>
             <h5>
               <h4 className ="region fancy-type">{ fish.region }</h4>
-              <p className = "description">Field Notes: {fish.description}</p>
-              <p className ="price fancy-type">${ fish.price }</p>
+              <p className="description"><span id="field-notes">Field Notes:</span> {fish.description}</p>
+              <p className ="price fancy-type yellow">${ fish.price }</p>
             </h5>
             <button className='btn btn-primary' id="product-add">
                 <span className="glyphicon glyphicon-shopping-cart"></span>
             </button>
-            <button className='btn btn-danger' id='product-remove'> X </button>
+            <button className='btn btn-danger' id='product-delete'> x </button>
           </div>
           <div className="reviews col-xs-12">
                 { reviews &&
