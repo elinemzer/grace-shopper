0cbed8ebc5d9b651348e4099e50b85a5ffefe582
Emily Grace Acres <acres.emilygrace@gmail.com>
2017-06-12 18:24:25 -0400

    added even more styling including a responsive design and a footer

--- browser/components/AllProducts.js
+++ browser/components/AllProducts.js
@@ -4,26 +4,35 @@ import { Link } from 'react-router';
 export default function AllProducts (props) {
   const region = props.routeParams.region;
   let fish;
+  let title;
   if (region) {
+    // splitting title on the space for 'north america' and 'south america'
+     if(region.indexOf('america') > 0){
+      title = region.slice(0, 5) + ' ' + region.slice(5);
+    } else {
+      title = region;
+    }
     fish = props.products.filter( fish => {
       return fish.region.split(' ').join('').toLowerCase() == region.toLowerCase();
     })
   } else {
     fish = props.products;
+    title = 'Our current selection';
   }
 
   return (
   	<div className="container-fluid default-container">
-      <h2 className="fancy-type" id="products-title">Our current selection</h2>
+      <h2 className="fancy-type" id="products-title">{ title }</h2>
       <div className="row">
       {
         fish && fish.map(oneFish => (
-          <div id="product-tile" className="col-xs-4" key={oneFish.id}>
+          <div id="product-tile" className="col-xs-12 col-sm-6 col-md-6 col-lg-4" key={ oneFish.id }>
             <Link className="thumbnail" to={`/products/${oneFish.id}`}>
               <img src={ oneFish.imageUrl } />
               <div className="caption">
-                  <p id="product-tile-title">{ oneFish.title } ({ oneFish.region })</p>
-                  <p className="yellow" id="product-tile-price">${oneFish.price}</p>
+                  <p id="product-tile-title">{ oneFish.title }</p>
+                  <p id="product-tile-subtitle">({ oneFish.region })</p>
+                  <p className="yellow" id="product-tile-price">${ oneFish.price }</p>
               </div>
             </Link>
           </div>
