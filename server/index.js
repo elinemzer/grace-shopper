4e49256c1578fd0997da8c83a80cdb92b5c9ee08
Eli Nemzer <elinemzer7@gmail.com>
2017-06-12 12:44:16 -0400

    updated app.listen

--- server/index.js
+++ server/index.js
@@ -83,7 +83,7 @@ app.use(function (err, req, res, next) {
 });
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("listening on port 3000");
 })
 module.exports = app;
